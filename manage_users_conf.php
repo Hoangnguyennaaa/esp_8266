48fb18ed45a3f8d314f03dc66757fc2bc2fdf737
Hoangnguyennaaa <113451387+Hoangnguyennaaa@users.noreply.github.com>
2022-09-13 22:13:38 +0700

    Add files via upload

---
+++ manage_users_conf.php
@@ -0,0 +1,330 @@
+<?php  
+//Connect to database
+require'connectDB.php';
+
+// select passenger 
+if (isset($_GET['select'])) {
+
+    $Finger_id = $_GET['Finger_id'];
+
+    $sql = "SELECT fingerprint_select FROM users WHERE fingerprint_select=1";
+    $result = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($result, $sql)) {
+        echo "SQL_Error_Select";
+        exit();
+    }
+    else{
+        mysqli_stmt_execute($result);
+        $resultl = mysqli_stmt_get_result($result);
+        if ($row = mysqli_fetch_assoc($resultl)) {
+
+            $sql="UPDATE users SET fingerprint_select=0";
+            $result = mysqli_stmt_init($conn);
+            if (!mysqli_stmt_prepare($result, $sql)) {
+                echo "SQL_Error_Select";
+                exit();
+            }
+            else{
+                mysqli_stmt_execute($result);
+
+                $sql="UPDATE users SET fingerprint_select=1 WHERE fingerprint_id=?";
+                $result = mysqli_stmt_init($conn);
+                if (!mysqli_stmt_prepare($result, $sql)) {
+                    echo "SQL_Error_select_Fingerprint";
+                    exit();
+                }
+                else{
+                    mysqli_stmt_bind_param($result, "s", $Finger_id);
+                    mysqli_stmt_execute($result);
+
+                    echo "Đã chọn vân tay này";
+                    exit();
+                }
+            }
+        }
+        else{
+            $sql="UPDATE users SET fingerprint_select=1 WHERE fingerprint_id=?";
+            $result = mysqli_stmt_init($conn);
+            if (!mysqli_stmt_prepare($result, $sql)) {
+                echo "SQL_Error_select_Fingerprint";
+                exit();
+            }
+            else{
+                mysqli_stmt_bind_param($result, "s", $Finger_id);
+                mysqli_stmt_execute($result);
+
+                echo "Vân tay học sinh đã chọn";
+                exit();
+            }
+        }
+    } 
+}
+if (isset($_POST['Add'])) {
+     
+    $Uname = $_POST['name'];
+    $Number = $_POST['number'];
+    $Email= $_POST['email'];
+
+    //optional
+    $Timein = $_POST['timein'];
+    $Gender= $_POST['gender'];
+
+    //check if there any selected user
+    $sql = "SELECT username FROM users WHERE fingerprint_select=1";
+    $result = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($result, $sql)) {
+      echo "SQL_Error";
+      exit();
+    }
+    else{
+        mysqli_stmt_execute($result);
+        $resultl = mysqli_stmt_get_result($result);
+        if ($row = mysqli_fetch_assoc($resultl)) {
+
+            if (empty($row['username'])) {
+
+                if (!empty($Uname) && !empty($Number) && !empty($Email)) {
+                    //check if there any user had already the Serial Number
+                    $sql = "SELECT serialnumber FROM users WHERE serialnumber=?";
+                    $result = mysqli_stmt_init($conn);
+                    if (!mysqli_stmt_prepare($result, $sql)) {
+                        echo "SQL_Error";
+                        exit();
+                    }
+                    else{
+                        mysqli_stmt_bind_param($result, "d", $Number);
+                        mysqli_stmt_execute($result);
+                        $resultl = mysqli_stmt_get_result($result);
+                        if (!$row = mysqli_fetch_assoc($resultl)) {
+                            $sql="UPDATE users SET username=?, serialnumber=?, gender=?, email=?, user_date=CURDATE(), time_in=? WHERE fingerprint_select=1";
+                            $result = mysqli_stmt_init($conn);
+                            if (!mysqli_stmt_prepare($result, $sql)) {
+                                echo "SQL_Error_select_Fingerprint";
+                                exit();
+                            }
+                            else{
+                                mysqli_stmt_bind_param($result, "sdsss", $Uname, $Number, $Gender, $Email, $Timein );
+                                mysqli_stmt_execute($result);
+
+                                echo "Học sinh mới đã được thêm!";
+                                exit();
+                            }
+                        }
+                        else {
+                            echo "Mã học sinh đã được thêm!";
+                            exit();
+                        }
+                    }
+                }
+                else{
+                    echo "Không bỏ phần này";
+                    exit();
+                }
+            }
+            else{
+                echo "Vân tay này đã được thêm vào";
+                exit();
+            }    
+        }
+        else {
+            echo "Không có vân tay nào được chọn!";
+            exit();
+        }
+    }
+}
+//Add user Fingerprint
+if (isset($_POST['Add_fingerID'])) {
+
+    $fingerid = $_POST['fingerid'];
+
+    if ($fingerid == 0) {
+        echo "Nhập mã vân tay!";
+        exit();
+    }
+    else{
+        if ($fingerid > 0 && $fingerid < 128) {
+            $sql = "SELECT fingerprint_id FROM users WHERE fingerprint_id=?";
+            $result = mysqli_stmt_init($conn);
+            if (!mysqli_stmt_prepare($result, $sql)) {
+              echo "SQL_Error";
+              exit();
+            }
+            else{
+                mysqli_stmt_bind_param($result, "i", $fingerid );
+                mysqli_stmt_execute($result);
+                $resultl = mysqli_stmt_get_result($result);
+                if (!$row = mysqli_fetch_assoc($resultl)) {
+
+                    $sql = "SELECT add_fingerid FROM users WHERE add_fingerid=1";
+                    $result = mysqli_stmt_init($conn);
+                    if (!mysqli_stmt_prepare($result, $sql)) {
+                      echo "SQL_Error";
+                      exit();
+                    }
+                    else{
+                        mysqli_stmt_execute($result);
+                        $resultl = mysqli_stmt_get_result($result);
+                        if (!$row = mysqli_fetch_assoc($resultl)) {
+                            $sql = "INSERT INTO users (fingerprint_id, add_fingerid) VALUES (?, 1)";
+                            $result = mysqli_stmt_init($conn);
+                            if (!mysqli_stmt_prepare($result, $sql)) {
+                              echo "SQL_Error";
+                              exit();
+                            }
+                            else{
+                                mysqli_stmt_bind_param($result, "i", $fingerid );
+                                mysqli_stmt_execute($result);
+                                echo "Mã này sẵn sàng nhận vân tay mới";
+                                exit();
+                            }
+                        }
+                        else{
+                            echo "Bạn không thể thêm nhiều mã cho mỗi lần";
+                        }
+                    }   
+                }
+                else{
+                    echo "Mã vân tay này đã tồn tại!";
+                    exit();
+                }
+            }
+        }
+        else{
+            echo "Mã vân tay chỉ giới hạn từ 1 đến 127";
+            exit();
+        }
+    }
+}
+// Update an existance user 
+if (isset($_POST['Update'])) {
+
+    $Uname = $_POST['name'];
+    $Number = $_POST['number'];
+    $Email= $_POST['email'];
+
+    //optional
+    $Timein = $_POST['timein'];
+    $Gender= $_POST['gender'];
+
+    if ($Number == 0) {
+        $Number = -1;
+    }
+    //check if there any selected user
+    $sql = "SELECT * FROM users WHERE fingerprint_select=1";
+    $result = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($result, $sql)) {
+      echo "SQL_Error";
+      exit();
+    }
+    else{
+        mysqli_stmt_execute($result);
+        $resultl = mysqli_stmt_get_result($result);
+        if ($row = mysqli_fetch_assoc($resultl)) {
+
+            if (empty($row['username'])) {
+                echo "Trước hết hãy thêm học sinh!";
+                exit();
+            }
+            else{
+                if (empty($Uname) && empty($Number) && empty($Email) && empty($Timein)) {
+                    echo "Không bỏ phần này";
+                    exit();
+                }
+                else{
+                    //check if there any user had already the Serial Number
+                    $sql = "SELECT serialnumber FROM users WHERE serialnumber=?";
+                    $result = mysqli_stmt_init($conn);
+                    if (!mysqli_stmt_prepare($result, $sql)) {
+                        echo "SQL_Error";
+                        exit();
+                    }
+                    else{
+                        mysqli_stmt_bind_param($result, "d", $Number);
+                        mysqli_stmt_execute($result);
+                        $resultl = mysqli_stmt_get_result($result);
+                        if (!$row = mysqli_fetch_assoc($resultl)) {
+
+                            if (!empty($Uname) && !empty($Email) && !empty($Timein)) {
+
+                                $sql="UPDATE users SET username=?, serialnumber=?, gender=?, email=?, time_in=? WHERE fingerprint_select=1";
+                                $result = mysqli_stmt_init($conn);
+                                if (!mysqli_stmt_prepare($result, $sql)) {
+                                    echo "SQL_Error_select_Fingerprint";
+                                    exit();
+                                }
+                                else{
+                                    mysqli_stmt_bind_param($result, "sdsss", $Uname, $Number, $Gender, $Email, $Timein );
+                                    mysqli_stmt_execute($result);
+
+                                    echo "Học sinh được chọn đã được cập nhật lại!";
+                                    exit();
+                                }
+                            }
+                            else{
+                                if (!empty($Timein)) {
+                                    $sql="UPDATE users SET gender=?, time_in=? WHERE fingerprint_select=1";
+                                    $result = mysqli_stmt_init($conn);
+                                    if (!mysqli_stmt_prepare($result, $sql)) {
+                                        echo "SQL_Error_select_Fingerprint";
+                                        exit();
+                                    }
+                                    else{
+                                        mysqli_stmt_bind_param($result, "ss", $Gender, $Timein );
+                                        mysqli_stmt_execute($result);
+
+                                        echo "Học sinh được chọn đã được cập nhật lại!";
+                                        exit();
+                                    }
+                                }
+                                else{
+                                    echo "Thời gian vào của học sinh này trống !";
+                                    exit();
+                                }    
+                            }  
+                        }
+                        else {
+                            echo "Mã học sinh đã được lấy!";
+                            exit();
+                        }
+                    }
+                }
+            }    
+        }
+        else {
+            echo "Không có học sinh nào được chọn cập nhật!";
+            exit();
+        }
+    }
+}
+// delete user 
+if (isset($_POST['delete'])) {
+
+    $sql = "SELECT fingerprint_select FROM users WHERE fingerprint_select=1";
+    $result = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($result, $sql)) {
+        echo "SQL_Error_Select";
+        exit();
+    }
+    else{
+        mysqli_stmt_execute($result);
+        $resultl = mysqli_stmt_get_result($result);
+        if ($row = mysqli_fetch_assoc($resultl)) {
+            $sql="UPDATE users SET username='', serialnumber='', gender='', email='', time_in='', del_fingerid=1 WHERE fingerprint_select=1";
+            $result = mysqli_stmt_init($conn);
+            if (!mysqli_stmt_prepare($result, $sql)) {
+                echo "SQL_Error_delete";
+                exit();
+            }
+            else{
+                mysqli_stmt_execute($result);
+                echo "Mã vân tay này đã được xoá";
+                exit();
+            }
+        }
+        else{
+            echo "Chon 1 người dùng để xoá";
+            exit();
+        }
+    }
+}
+?>
